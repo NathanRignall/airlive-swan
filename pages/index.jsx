7afbffc9e9281318a73fbffab65b72127e593d8b
Nathan Rignall <rignallnathan@gmail.com>
2021-06-17 15:05:07 +0100

    feat(navbar): added old navbar
    also fixed issue with jit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
 import Header from "components/parts/header";
+import Navbar from "components/parts/navbar";
 
 export default function Home() {
   return (
     <>
       <Header title="AirLive Home" />
+      <Navbar />
     </>
   );
 }
