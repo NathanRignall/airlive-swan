21191b0c95805ae5df5ec9a89f977f35cff771ef
Nathan Rignall <rignallnathan@gmail.com>
2021-06-17 18:07:33 +0100

    feat(index): added index page
    also removed theming to test file

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,24 @@ import Header from "components/parts/header";
 import Navbar from "components/parts/navbar";
 
 export default function Home() {
-  return (
-    <>
-      <Header title="AirLive Home" />
-      <Navbar />
-    </>
-  );
+    return (
+        <>
+            <Header title="AirLive Home" />
+            <Navbar />
+
+            <div className="flex flex-col min-h-screen ">
+                <section className="flex bg-gradient-to-r from-green-100 to-blue-100 py-20 text-center dark:from-green-800 dark:to-blue-800">
+                    <div className="m-auto pt-32">
+                        <h1 className="lg:text-10xl md:text-10xl sm:text-9xl text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-logo-light to-logo-dark dark:from-logo-light-light dark:to-logo-dark-light">
+                            AirLive
+                        </h1>
+                        <p className="md:text-5xl text-4xl font-bold mb-8 text-gray-700 dark:text-white">
+                            Simple Live Broadcasts
+                        </p>
+                    </div>
+                </section>
+                <div className="mx-auto px-4 pt-24 flex-grow "></div>
+            </div>
+        </>
+    );
 }
--- pages/test.jsx
+++ pages/test.jsx
@@ -2,18 +2,18 @@ import Head from "next/head";
 import { Button, Card } from "../components/basic";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 max-w-lg mx-auto">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <Button size={Button.size.LARGE}>Button</Button>
-      <Button variant={Button.variant.BLUE}>Button</Button>
-      <Button size={Button.size.SMALL}>Button</Button>
+            <Button size={Button.size.LARGE}>Button</Button>
+            <Button variant={Button.variant.BLUE}>Button</Button>
+            <Button size={Button.size.SMALL}>Button</Button>
 
-      <Card>Test Card here for testing perpouses</Card>
-    </div>
-  );
+            <Card>Test Card here for testing perpouses</Card>
+        </div>
+    );
 }
