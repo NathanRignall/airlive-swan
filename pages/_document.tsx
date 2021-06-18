24b606c4611f4fd5934e71070c34ddaf69a54a2e
Nathan Rignall <rignallnathan@gmail.com>
2021-06-18 23:06:33 +0100

    refactor(header): change where css is loaded

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head />
-                <body className="bg-gray-100 dark:bg-gray-600">
+                <body className="bg-gray-100 dark:bg-gray-700">
                     <Main />
                     <NextScript />
                 </body>
